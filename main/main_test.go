f98c4a360791bb0cb3c9420815f042ca12df30fc
Monish <monish.a@captainfresh.in>
2022-05-05 14:17:16 +0530

    Initial commit

---
+++ main/main_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	type args struct {
+		a int64
+		b int64
+	}
+	var tests []struct {
+		name string
+		args args
+		want int64
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Add(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
